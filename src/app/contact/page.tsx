0201762039469926798400b8a875fe97d272ef55
Technoblade <william@williamgiles.co.nz>
2024-11-07 18:04:10 +1300

    footer

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,6 +6,8 @@ import Nav from "~/components/Nav";
 
 import { toast } from "react-toastify";
 import ToastOptions from "~/utils/toastOptions";
+import { Card, CardContent } from "~/components/ui/card";
+import Footer from "~/components/footer";
 
 type Matrix<N extends number, M extends number> = number[][] & { length: N } & {
   [K in keyof number[][]]: { length: M };
@@ -94,47 +96,48 @@ export default function Contact() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-gray-50">
       <Nav />
+      <Card className="m-4 transition-shadow hover:shadow-lg">
+        <CardContent>
+          <div className="mx-auto grid max-w-4xl items-start gap-16 p-4 font-[sans-serif] md:grid-cols-2">
+            <div>
+              <h1 className="text-3xl font-extrabold text-gray-800">
+                Let&apos;s Talk
+              </h1>
+              <p className="mt-4 text-sm text-gray-500">
+                Have some big idea or brand to develop and need help? Then reach
+                out we&apos;d love to hear about your project and provide help.
+              </p>
 
-      <div className="mx-auto mt-24 grid max-w-4xl items-start gap-16 p-4 font-[sans-serif] md:grid-cols-2">
-        <div>
-          <h1 className="text-3xl font-extrabold text-gray-800">
-            Let&apos;s Talk
-          </h1>
-          <p className="mt-4 text-sm text-gray-500">
-            Have some big idea or brand to develop and need help? Then reach out
-            we&apos;d love to hear about your project and provide help.
-          </p>
+              <div className="mt-12">
+                <h2 className="text-base font-bold text-gray-800">Email</h2>
+                <ul className="mt-4">
+                  <li className="flex items-center">
+                    <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-[#e6e6e6cf]">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20px"
+                        height="20px"
+                        fill="#007bff"
+                        viewBox="0 0 479.058 479.058"
+                      >
+                        <path
+                          d="M434.146 59.882H44.912C20.146 59.882 0 80.028 0 104.794v269.47c0 24.766 20.146 44.912 44.912 44.912h389.234c24.766 0 44.912-20.146 44.912-44.912v-269.47c0-24.766-20.146-44.912-44.912-44.912zm0 29.941c2.034 0 3.969.422 5.738 1.159L239.529 264.631 39.173 90.982a14.902 14.902 0 0 1 5.738-1.159zm0 299.411H44.912c-8.26 0-14.971-6.71-14.971-14.971V122.615l199.778 173.141c2.822 2.441 6.316 3.655 9.81 3.655s6.988-1.213 9.81-3.655l199.778-173.141v251.649c-.001 8.26-6.711 14.97-14.971 14.97z"
+                          data-original="#000000"
+                        />
+                      </svg>
+                    </div>
+                    <a
+                      href="mailto:info@dcralph.com"
+                      className="ml-4 text-sm text-[#007bff]"
+                    >
+                      <small className="block">Mail</small>
+                      <strong>info@dcralph.com</strong>
+                    </a>
+                  </li>
+                </ul>
+              </div>
 
-          <div className="mt-12">
-            <h2 className="text-base font-bold text-gray-800">Email</h2>
-            <ul className="mt-4">
-              <li className="flex items-center">
-                <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-[#e6e6e6cf]">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20px"
-                    height="20px"
-                    fill="#007bff"
-                    viewBox="0 0 479.058 479.058"
-                  >
-                    <path
-                      d="M434.146 59.882H44.912C20.146 59.882 0 80.028 0 104.794v269.47c0 24.766 20.146 44.912 44.912 44.912h389.234c24.766 0 44.912-20.146 44.912-44.912v-269.47c0-24.766-20.146-44.912-44.912-44.912zm0 29.941c2.034 0 3.969.422 5.738 1.159L239.529 264.631 39.173 90.982a14.902 14.902 0 0 1 5.738-1.159zm0 299.411H44.912c-8.26 0-14.971-6.71-14.971-14.971V122.615l199.778 173.141c2.822 2.441 6.316 3.655 9.81 3.655s6.988-1.213 9.81-3.655l199.778-173.141v251.649c-.001 8.26-6.711 14.97-14.971 14.97z"
-                      data-original="#000000"
-                    />
-                  </svg>
-                </div>
-                <a
-                  href="mailto:info@dcralph.com"
-                  className="ml-4 text-sm text-[#007bff]"
-                >
-                  <small className="block">Mail</small>
-                  <strong>info@dcralph.com</strong>
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          {/* <div className="mt-12">
+              {/* <div className="mt-12">
             <h2 className="text-base font-bold text-gray-800">Socials</h2>
 
             <ul className="mt-4 flex space-x-4">
@@ -185,52 +188,56 @@ export default function Contact() {
               </li>
             </ul>
           </div> */}
-        </div>
+            </div>
 
-        <form className="ml-auto flex w-full flex-col items-center space-y-4">
-          <input
-            type="text"
-            placeholder="Name"
-            className="w-full rounded-md bg-gray-100 px-4 py-3 text-sm text-gray-800 outline-blue-500 focus:bg-transparent"
-            value={formName}
-            onChange={(e) => setFormName(e.target.value)}
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            className="w-full rounded-md bg-gray-100 px-4 py-3 text-sm text-gray-800 outline-blue-500 focus:bg-transparent"
-            value={formEmail}
-            onChange={(e) => setFormEmail(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Subject"
-            className="w-full rounded-md bg-gray-100 px-4 py-3 text-sm text-gray-800 outline-blue-500 focus:bg-transparent"
-            value={formSubject}
-            onChange={(e) => setFormSubject(e.target.value)}
-          />
-          <textarea
-            placeholder="Message"
-            rows={6}
-            className="w-full rounded-md bg-gray-100 px-4 pt-3 text-sm text-gray-800 outline-blue-500 focus:bg-transparent"
-            value={formMessage}
-            onChange={(e) => setFormMessage(e.target.value)}
-          ></textarea>
+            <form className="ml-auto flex w-full flex-col items-center space-y-4">
+              <input
+                type="text"
+                placeholder="Name"
+                className="w-full rounded-md bg-gray-100 px-4 py-3 text-sm text-gray-800 outline-blue-500 focus:bg-transparent"
+                value={formName}
+                onChange={(e) => setFormName(e.target.value)}
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                className="w-full rounded-md bg-gray-100 px-4 py-3 text-sm text-gray-800 outline-blue-500 focus:bg-transparent"
+                value={formEmail}
+                onChange={(e) => setFormEmail(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Subject"
+                className="w-full rounded-md bg-gray-100 px-4 py-3 text-sm text-gray-800 outline-blue-500 focus:bg-transparent"
+                value={formSubject}
+                onChange={(e) => setFormSubject(e.target.value)}
+              />
+              <textarea
+                placeholder="Message"
+                rows={6}
+                className="w-full rounded-md bg-gray-100 px-4 pt-3 text-sm text-gray-800 outline-blue-500 focus:bg-transparent"
+                value={formMessage}
+                onChange={(e) => setFormMessage(e.target.value)}
+              ></textarea>
 
-          <FakeCaptcha
-            image="/captcha/1.jpg" // Replace with your desired image URL
-            title={captchaTitle ?? ""}
-            onSubmit={handleCaptchaSubmit}
-          />
-          <button
-            type="button"
-            className="!mt-6 w-full rounded-md bg-blue-500 px-4 py-3 text-sm tracking-wide text-white hover:bg-blue-600"
-            onClick={submitForm}
-          >
-            Send
-          </button>
-        </form>
-      </div>
+              <FakeCaptcha
+                image="/captcha/1.jpg" // Replace with your desired image URL
+                title={captchaTitle ?? ""}
+                onSubmit={handleCaptchaSubmit}
+              />
+              <button
+                type="button"
+                className="!mt-6 w-full rounded-md bg-blue-500 px-4 py-3 text-sm tracking-wide text-white hover:bg-blue-600"
+                onClick={submitForm}
+              >
+                Send
+              </button>
+            </form>
+          </div>
+        </CardContent>
+      </Card>
+
+      <Footer />
     </main>
   );
 }
